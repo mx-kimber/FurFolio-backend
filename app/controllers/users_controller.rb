6d88aeb9c51f0881335e7afde73c4b3d9329047a
Kimber Ogden <mx.kimber@gmail.com>
2024-08-28 02:28:14 -0500

    Set current user and authorization in application controller, clean up code from scaffold in Users controller, set authorizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,73 +1,45 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+  before_action :authenticate_user, except: [:create]
 
-  # GET /users or /users.json
   def index
     @users = User.all
+    render json: @users
   end
 
-  # GET /users/1 or /users/1.json
   def show
+    render json: current_user
   end
 
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render :show, status: :created
+      render json: @user, status: :created
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
- 
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if current_user.update(user_params)
+      render json: current_user, status: :ok
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
+    if @user = current_user
+      @user.destroy
+      render json: { message: "User destroyed successfully" }
+    else
+      render json: { error: "User not found" }, status: :not_found
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.permit(:handle, :last_name, :first_name, :email,:password, :password_confirmation)
-    end
+  def user_params
+    params.permit(:handle, :last_name, :first_name, :email, :password, :password_confirmation)
+  end
+  
 end
